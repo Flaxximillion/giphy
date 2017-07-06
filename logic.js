74741ccca8df11c6176e3d1e564a763813ce5d7f
Flaxximillion <ayu2013@gmail.com>
2017-07-05 21:45:31 -0500

    Final logic

---
+++ logic.js
@@ -0,0 +1,64 @@
+const categories = ["Cats", "Cakes", "Cookies", "Corvettes", "Corgis", "Coffee", "Corn", "Cornucopias",
+"Corks", "Camouflage", "Carpets", "Creeks", "Cards", "Carols", "Christmas"];
+const buttonHolder = $("#buttonHolder");
+const gifHolder = $("#gifHolder");
+
+function genButtons(categories){
+    categories.forEach(function(category){
+        $("<button>").addClass("btn btn--md btn--blue")
+            .text(category)
+            .val(category)
+            .prependTo(buttonHolder)
+            .click(e=> {
+                $(gifHolder).children().remove();
+                getGiphy($(e.currentTarget).val(), 10);
+            });
+    });
+}
+
+function getGiphy(search, limit){
+    let url = "https://api.giphy.com/v1/gifs/search";
+    let parameters ={
+        "q": search,
+        "api_key": "1f8ff565529d4b1086779febdb9eb7cf",
+        "limit": limit
+    };
+
+    url += "?" + $.param(parameters);
+
+    axios.get(url)
+        .then(function(response){
+            displayGifs(response.data.data);
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+}
+
+function displayGifs(gifData){
+    console.log(gifData);
+    gifData.forEach(function(gif){
+        $(gifHolder).append($("<div>")
+            .append($("<img>")
+                .attr({
+                    "src": gif.images["fixed_height_small_still"].url,
+                    "alt": "gif of" + gif.slug,
+                    "class": "gif",
+                    "data-switch": gif.images["fixed_height_downsampled"].url
+                }),
+                $("<div>").text(gif.rating)));
+    })
+}
+
+$(document).ready(function(){
+    genButtons(categories);
+    $("#resetButton").click(e=>{
+        $(gifHolder).children().remove();
+    })
+});
+
+$(document).on("click", ".gif", function(){
+    let switchState = $(this).attr("src");
+    this.src = $(this).attr("data-switch");
+    $(this).attr("data-switch", switchState);
+});
